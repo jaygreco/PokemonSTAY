29bc2a075ee8e5aa6c2a08f7046709c6f70ae6a4
Jay Greco <jayv.greco@gmail.com>
2016-07-29 17:52:11 -0600

    Added colored app icons

--- gogo.sh
+++ gogo.sh
@@ -15,6 +15,17 @@ cp embedded.mobileprovision Payload/pokemongo.app/embedded.mobileprovision
 #copy edited info plist
 cp Info_edited.plist Payload/pokemongo.app/Info.plist
 
+#copy app icons to Payload/pokemongo.app/
+command cp -R AppIcons/AppIcon57x57.png Payload/pokemongo.app/
+command cp -R AppIcons/AppIcon57x57@2x.png Payload/pokemongo.app/
+command cp -R AppIcons/AppIcon60x60@2x.png Payload/pokemongo.app/
+command cp -R AppIcons/AppIcon60x60@3x.png Payload/pokemongo.app/
+command cp -R AppIcons/AppIcon72x72@2x~ipad.png Payload/pokemongo.app/
+command cp -R AppIcons/AppIcon72x72~ipad.png Payload/pokemongo.app/
+command cp -R AppIcons/AppIcon76x76@2x~ipad.png Payload/pokemongo.app/
+command cp -R AppIcons/AppIcon76x76~ipad.png Payload/pokemongo.app/
+command cp -R AppIcons/AppIcon83.5x83.5@2x~ipad.png Payload/pokemongo.app/
+
 # zip to ipa
 zip -qr pokemongo.ipa Payload
 
